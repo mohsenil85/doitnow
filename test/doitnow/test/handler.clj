16ed19f469f844a11104a3e5be268a2ea27df226
Logan Mohseni <mohsenil85@gmail.com>
2013-09-29 17:23:10 -0700

    init

---
+++ test/doitnow/test/handler.clj
@@ -0,0 +1,14 @@
+(ns doitnow.test.handler
+  (:use clojure.test
+        ring.mock.request  
+        doitnow.handler))
+
+(deftest test-app
+  (testing "main route"
+    (let [response (app (request :get "/"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "Hello World"))))
+  
+  (testing "not-found route"
+    (let [response (app (request :get "/invalid"))]
+      (is (= (:status response) 404)))))
